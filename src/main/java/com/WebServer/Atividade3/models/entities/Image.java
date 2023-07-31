a1e3cc014fb8b07958e087071e155679f9f5f15a
Marcos <marcospegodesousa10@gmail.com>
2023-07-31 08:48:05 -0300

    2

--- src/main/java/com/WebServer/Atividade3/models/entities/Image.java
+++ src/main/java/com/WebServer/Atividade3/models/entities/Image.java
@@ -2,19 +2,20 @@ package com.WebServer.Atividade3.models.entities;
 
 import java.util.Arrays;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Lob;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 
 @Entity
 public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Lob
+    @Column(length=512)
     private byte[] data;
 
     public Long getId() {
